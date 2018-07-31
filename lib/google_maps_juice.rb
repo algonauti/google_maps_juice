5794c0990b650357c75ccaa509cc8d461319e1bf
Davide Papagni <davide@algonauti.com>
2018-07-31 10:51:15 +0200

    active_support: use core object extensions and autoload

--- lib/google_maps_juice.rb
+++ lib/google_maps_juice.rb
@@ -1,6 +1,9 @@
+require 'active_support/core_ext/object'
+require 'active_support/dependencies/autoload'
+
 require 'google_maps_juice/configuration'
 require 'google_maps_juice/version'
 
 module GoogleMapsJuice
-  # Your code goes here...
+  extend ActiveSupport::Autoload
 end

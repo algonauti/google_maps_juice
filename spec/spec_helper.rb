9758d5f3b13931449fed25e93a4b4412837d3644
Davide Papagni <davide@algonauti.com>
2018-08-01 19:08:08 +0200

    spec: fixture helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,12 @@ GoogleMapsJuice.configure do |config|
   config.api_key = 'dummy_api_key'
 end
 
+module FixtureHelpers
+  def response_fixture(name)
+    File.open("spec/fixtures/response/#{name}.json").read
+  end
+end
+
 RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
@@ -13,4 +19,6 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+
+  config.include FixtureHelpers
 end

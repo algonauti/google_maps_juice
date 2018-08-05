c9d65747badc1ff9c5045f6975827a7ff1b47957
Davide Papagni <davide@algonauti.com>
2018-08-05 14:46:40 +0200

    move Geocoding::Reponse to separate file

--- lib/google_maps_juice/geocoding.rb
+++ lib/google_maps_juice/geocoding.rb
@@ -1,4 +1,4 @@
-require 'active_support/core_ext/hash/slice'
+require 'google_maps_juice/geocoding/response'
 
 module GoogleMapsJuice
   class Geocoding < Endpoint
@@ -38,58 +38,6 @@ module GoogleMapsJuice
       unless required_params_present
         raise ArgumentError, "One of the following params is required: #{required_params.join(', ')}"
       end
-    end
-
-
-    class Response < GoogleMapsJuice::Endpoint::Response
-
-      def latitude
-        location['lat']
-      end
-
-      def longitude
-        location['lng']
-      end
-
-      def location
-        result.dig('geometry', 'location')
-      end
-
-      def partial_match?
-        result['partial_match'] == true
-      end
-
-      def result
-        results.first
-      end
-
-      def address_components
-        result['address_components']
-      end
-
-
-      %w( street_number
-          route
-          locality
-          postal_code
-          administrative_area_level_1
-          administrative_area_level_2
-          administrative_area_level_3
-          country ).each do |type|
-
-        define_method(type) do
-          addr_component_by(type: type)&.slice('long_name', 'short_name')
-        end
-
-      end
-
-
-      private
-
-      def addr_component_by(type: '')
-        address_components.find { |ac| ac['types'].include?(type) }
-      end
-
     end
 
   end
---
+++ lib/google_maps_juice/geocoding/response.rb
@@ -0,0 +1,54 @@
+require 'active_support/core_ext/hash/slice'
+
+module GoogleMapsJuice
+  class Response < GoogleMapsJuice::Endpoint::Response
+
+    def latitude
+      location['lat']
+    end
+
+    def longitude
+      location['lng']
+    end
+
+    def location
+      result.dig('geometry', 'location')
+    end
+
+    def partial_match?
+      result['partial_match'] == true
+    end
+
+    def result
+      results.first
+    end
+
+    def address_components
+      result['address_components']
+    end
+
+
+    %w( street_number
+        route
+        locality
+        postal_code
+        administrative_area_level_1
+        administrative_area_level_2
+        administrative_area_level_3
+        country ).each do |type|
+
+      define_method(type) do
+        addr_component_by(type: type)&.slice('long_name', 'short_name')
+      end
+
+    end
+
+
+    private
+
+    def addr_component_by(type: '')
+      address_components.find { |ac| ac['types'].include?(type) }
+    end
+
+  end
+end
